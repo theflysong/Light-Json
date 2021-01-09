7112f3b903c3b32fb79a88d5ffe573f3fca9b3ca
flysong <song_of_the_fly@163.com>
2021-01-09 23:18:54 +0800

    Complete Lexer

--- README.md
+++ README.md
@@ -3,8 +3,13 @@ LightWeight-Json
 
 * Only One File
 * Not too much functions
-* Only to add
+* Only to do
+    add
     ```
     #define LJSON_ASHEADER
-    #include "light-json.h"
+    #include "light-json.cpp"
     ```
+    to your code
+    add "light-json.cpp"
+    to your project
+* Attention: should use flag std=c++11
--- light-json.cpp
+++ light-json.cpp
@@ -1,3 +1,5 @@
+#ifndef __LIGHT_JSON_HEADER__ONCE_
+#define __LIGHT_JSON_HEADER__ONCE_
 #include <iostream>
 #include <string>
 #include <vector>
@@ -12,13 +14,84 @@ struct Token {
 		CUR_BR,
 		MID_BR,
 		/*These In ASTTree*/
-		NEXT_LEVEL,
+		OBJECT,
+		ARRAY,
 		PAIR,
 		EMPTY
 	};
 	Type type;
 	string value;
 };
+
+
+struct Lexer {
+	vector<Token> output;
+	void lex(string code);
+};
+
+string toString(Token::Type type);
+
+#ifndef LJSON_ASHEADER 
+
+void Lexer::lex(string code) {
+	for (int i = 0 ; i < code.length() ; i ++) {
+		switch (code[i]) {
+		case ' ':
+		case '\n':
+		case '\r':
+		case '\t':
+			break;
+		case '{': {
+			output.push_back(Token{Token::CUR_BR, "{"});
+			break;
+		}
+		case '}': {
+			output.push_back(Token{Token::CUR_BR, "}"});
+			break;
+		}
+		case '[': {
+			output.push_back(Token{Token::MID_BR, "["});
+			break;
+		}
+		case ']': {
+			output.push_back(Token{Token::MID_BR, "]"});
+			break;
+		}
+		case ':': {
+			output.push_back(Token{Token::COLON, ":"});
+			break;
+		}
+		case ',': {
+			output.push_back(Token{Token::COMMA, ","});
+			break;
+		}
+		case '0':
+		case '1':
+		case '2':
+		case '3':
+		case '4':
+		case '5':
+		case '6':
+		case '7':
+		case '8':
+		case '9': {
+			string v = "";
+			while (isdigit(code[i])) v += code[i ++];
+			i --;
+			output.push_back(Token{Token::NUMBER, v});
+			break;
+		}
+		case '"': {
+			string v = ""; i ++;
+			while (code[i] != '"') v += code[i ++]; 
+			output.push_back(Token{Token::STRING, v});
+		}
+		default: {
+			break;
+		}
+	}
+	}
+}
 
 string toString(Token::Type type) {
 	switch (type) {
@@ -34,10 +107,12 @@ string toString(Token::Type type) {
 		return "Curly Brace";
 	case Token::MID_BR:
 		return "Middle Brace";
-	case Token::NEXT_LEVEL:
-		return "Next";
+	case Token::OBJECT:
+		return "Object";
+	case Token::ARRAY:
+		return "Array";
 	case Token::PAIR:
-		return "Key-Value Pair";
+		return "Pair";
 	case Token::EMPTY:
 		return "Empty";
 	default:
@@ -45,40 +120,21 @@ string toString(Token::Type type) {
 	}
 }
 
-struct Lexer {
-	vector<Token> output;
-	void lex(string code) {
-		for (int i = 0 ; i < code.length() ; i ++) {
-			switch (code[i]) {
-			case ' ':
-			case '\n':
-			case '\r':
-			case '\t':
-				break;
-			case '{': {
-				output.push_back(Token{Token::CUR_BR, "{"});
-				break;
-			}
-			case '}': {
-				output.push_back(Token{Token::CUR_BR, "}"});
-				break;
-			}
-			case '[': {
-				output.push_back(Token{Token::MID_BR, "["});
-				break;
-			}
-			case ']': {
-				output.push_back(Token{Token::MID_BR, "]"});
-				break;
-			}
-			default: {
-				break;
-			}
-			}
-		}
-	}
-};
-
+#define __DEBUG__LJSON_
+#ifdef __DEBUG__LJSON_
 int main() {
+	Lexer lexer;
+	lexer.lex(
+		"{\n\
+			\"a\":233,\n\
+			\"c\": {\"arr\": [\"222\", \"777\"]}\n\
+		}"
+		);
+	for(Token token : lexer.output) {
+		cout << toString(token.type) << ":" << token.value << endl;
+	}
 	return 0;
 }
+#endif
+#endif
+#endif
